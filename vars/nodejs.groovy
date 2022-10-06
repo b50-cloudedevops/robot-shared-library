e19ca4c781cfe2b44623d13d10a99fc14703b398
vamsi434 <vamsibusa@gmail.com>
2022-10-06 18:00:23 +0530

    thsg

---
+++ vars/nodejs.groovy
@@ -0,0 +1,32 @@
+def lintCheck() {
+sh '''
+     # We want devs to handle the lint checks failure
+     # npm i jslint
+     # node_modules/jslint/bin/jslint.js server.js || true
+     echo starting lint checks
+     echo lint checks completed
+ '''
+
+}
+
+def call() {
+   pipeline {
+     agent any
+      stages {
+        stage('Installing the node js dependencies') {
+            steps {
+                sh "npm install"
+            }
+        }
+        stage('Lint checks') {
+            steps {
+                script {
+                    nodejs.lintCheck()
+                }
+            }
+
+        }
+    }
+}
+}
+// call is the default function whicch will be called
