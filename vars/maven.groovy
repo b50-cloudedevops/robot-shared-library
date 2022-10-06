a2d4d0d0a6d741d52a530f9998e6d7ce9b29fa08
vamsi434 <vamsibusa@gmail.com>
2022-10-06 22:00:15 +0530

    bxn

---
+++ vars/maven.groovy
@@ -0,0 +1,25 @@
+def lintCheck() {
+sh '''
+     echo Starting lint checks 
+     mvn checkstyle:check || true
+     echo lint checks completed for ${COMPONENT}
+ '''
+
+}
+
+def call() {
+   pipeline {
+     agent any
+      stages {
+        stage('Lint checks') {
+            steps {
+                script {
+                    lintCheck()
+                }
+            }
+
+        }
+    }
+}
+}
+// call is the default function whicch will be called
