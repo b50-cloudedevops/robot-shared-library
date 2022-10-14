5ec1ca45e6172bb307bad7e5e99aa59a3336f3fb
vamsi434 <vamsibusa@gmail.com>
2022-10-14 16:43:31 +0530

    ppl

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -26,7 +26,7 @@ def call() {
             steps {
                 script {
                     sh "mvn clean package"
-                    env.ARGS='-Dsonar.sources=target/'
+                    env.ARGS='-Dsonar.java.binaries=target/'
                     common.sonarCheck()
                 }
             }
