671cb7db52a5700ad36a1d1f3a8baef701cae31c
vamsi434 <vamsibusa@gmail.com>
2022-10-14 18:42:27 +0530

    thz

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def sonarCheck() {
     sh '''
-          sonar-scanner -Dsonar.host.url=http://172.31.1.192:9000/  -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.java.binaries=target/
+          sonar-scanner -Dsonar.host.url=http://172.31.1.192:9000/  -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} 
           sonar-quality-gate ${SONAR_USR} ${SONAR_PSW} 172.31.1.192 ${COMPONENT} || true // This gives the result of the scan based on that it will abort pipeline or proceed further    
           echo sonarchecks for ${COMPONENT}    
     '''
