eda9affa8b634114ecf660ed59939d00a62f8f19
vamsi434 <vamsibusa@gmail.com>
2022-10-14 12:03:19 +0530

    lk

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -10,6 +10,9 @@ sh '''
 def call() {
    pipeline {
      agent any
+   environment {
+    SONAR=credentials('Sonar')
+ }
       stages {
         stage('Lint checks') {
             steps {
@@ -20,6 +23,34 @@ def call() {
 
         }
          
+          tage('sonar check') {
+            steps {
+                script {
+                    sh "mvn clean compile"
+                    env.ARGS = "-Dsonar.sources=target/"
+                    common.sonarCheck()
+                }
+            }
+        }  
+        stage('test cases') {
+            parallel {
+                stage('unit Tests') {
+                    steps {
+                        sh "echo Unit test cases completed"
+                    }
+                }
+                stage('Integration Tests') {
+                    steps {
+                        sh "echo Integration test cases completed"
+                    }
+                }
+                stage('Functional Tests') {
+                    steps {
+                        sh " echo Functional test cases completed"
+                    }
+                }
+            }
+        } 
     }
  
   }
