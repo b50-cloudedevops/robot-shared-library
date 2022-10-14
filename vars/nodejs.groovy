a854a8bd0d4715f58c2a6f4080e211c2c364daf0
vamsi434 <vamsibusa@gmail.com>
2022-10-14 19:09:22 +0530

    hsb

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -52,7 +52,10 @@ def call() {
                         sh " echo Functional test cases completed"
                     }
                 }
-                stage('Prepare artifacts') {
+            }
+        }
+
+        stage('Prepare artifacts') {
                  when {
                     expression { env.TAG_NAME != null }
                  }
@@ -62,8 +65,8 @@ def call() {
                         zip ${COMPONENT}.zip node_modules server.js
                     '''
                   }
-                }
-                stage('Upload artifacts') {
+           }
+        stage('Upload artifacts') {
                  when {
                     expression { env.TAG_NAME != null }
                  }
@@ -75,7 +78,6 @@ def call() {
             }
          
         }
-    }
-    }
+    
     }
 }
