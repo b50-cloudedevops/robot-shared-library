6de6bec773d629c2810092d02747f3b1a03289bf
vamsi434 <vamsibusa@gmail.com>
2022-10-14 11:34:14 +0530

    pass

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,7 +11,7 @@ sh '''
 
 def sonarCheck() {
     sh '''
-         sonar-scanner -Dsonar.host.url=http://172.31.1.192:9000/  -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT}
+         sonar-scanner -Dsonar.host.url=http://172.31.1.192:9000/  -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=Password
     '''
 }
 def call() {
