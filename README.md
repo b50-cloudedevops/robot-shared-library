ee47211c9e2df0c90d40f10cf9eb7399e0515a55
vamsi434 <vamsibusa@gmail.com>
2022-10-14 09:50:05 +0530

    thsg

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # robot-shared-library
+//stage ('sonar check') {
+        //    steps {
+        //        script {
+        //            env.ARGS = "-Dsonar.sources=."
+        //            common.sonarCheck()
+        //        }
+        //    }
+        //}
+        //stage ('test cases') {
+        //    parallel {
+        //        stage('unit Tests') {
+        //            steps {
+        //                sh "echo Unit test cases completed"
+        //            }
+        //        }
+        //        stage ('Integration Tests') {
+        //            steps {
+        //                sh "echo Integration test cases completed"
+        //            }
+        //        }
+        //        stage('Functional Tests') {
+        //            steps {
+        //                sh " echo Functional test cases completed"
+        //            }
+       //         }
+       //     }
+        // }
+        //    stage('Prepare artifacts') {
+        //        when {
+        //            expression { env.TAG_NAME != null }
+        //        }
+        //        steps {
+        //            sh '''
+        //                npm install
+        //                zip ${COMPONENT}.zip node_modules server.js
+        //            '''
+        //        }
+        //    }
+        //    stage('Upload artifacts') {
+        //        when {
+        //            expression { env.TAG_NAME != null }
+        //        }
+        //        steps {
+        //            sh '''
+        //               curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file pom.xml http://172.31.3.52:8081/repository/${COMPONENT}/${COMPONENT}.zip
+        //            '''
+        //        }
+        //    }
+        //} 
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,11 +12,11 @@ sh '''
 def call() {
  pipeline {
      agent any
-      environment {
+       environment {
         SONAR = credentials('Sonar')
         NEXUS = credentials('NEXUS')
-       }
-      stages{
+        }
+       stages{
          stage('Installing the node js dependencies') {
             steps {
                 sh "npm install"
@@ -28,57 +28,7 @@ def call() {
                     lintCheck()
                 }
             }
-
         }
-        //stage ('sonar check') {
-        //    steps {
-        //        script {
-        //            env.ARGS = "-Dsonar.sources=."
-        //            common.sonarCheck()
-        //        }
-        //    }
-        //}
-        //stage ('test cases') {
-        //    parallel {
-        //        stage('unit Tests') {
-        //            steps {
-        //                sh "echo Unit test cases completed"
-        //            }
-        //        }
-        //        stage ('Integration Tests') {
-        //            steps {
-        //                sh "echo Integration test cases completed"
-        //            }
-        //        }
-        //        stage('Functional Tests') {
-        //            steps {
-        //                sh " echo Functional test cases completed"
-        //            }
-       //         }
-       //     }
-        // }
-        //    stage('Prepare artifacts') {
-        //        when {
-        //            expression { env.TAG_NAME != null }
-        //        }
-        //        steps {
-        //            sh '''
-        //                npm install
-        //                zip ${COMPONENT}.zip node_modules server.js
-        //            '''
-        //        }
-        //    }
-        //    stage('Upload artifacts') {
-        //        when {
-        //            expression { env.TAG_NAME != null }
-        //        }
-        //        steps {
-        //            sh '''
-        //               curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file pom.xml http://172.31.3.52:8081/repository/${COMPONENT}/${COMPONENT}.zip
-        //            '''
-        //        }
-        //    }
-        //} 
-   }
- 
+       }
+ }
 }
