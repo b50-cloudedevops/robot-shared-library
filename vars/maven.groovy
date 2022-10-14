0d704b303fc7104754862b734675bfe098c93a40
vamsi434 <vamsibusa@gmail.com>
2022-10-14 12:28:14 +0530

    lint

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -31,27 +31,8 @@ def call() {
                 }
             }
         }  
-        stage('test cases') {
-            parallel {
-                stage('unit Tests') {
-                    steps {
-                        sh "echo Unit test cases completed"
-                    }
-                }
-                stage('Integration Tests') {
-                    steps {
-                        sh "echo Integration test cases completed"
-                    }
-                }
-                stage('Functional Tests') {
-                    steps {
-                        sh " echo Functional test cases completed"
-                    }
-                }
-            }
-        } 
-    }
- 
-  }
+        
+      }
+   }
+
 }
-// call is the default function whicch will be called
