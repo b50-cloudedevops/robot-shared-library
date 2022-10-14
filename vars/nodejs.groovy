3596c5520fd0e7220a69d44d958deac46a40ab9f
vamsi434 <vamsibusa@gmail.com>
2022-10-14 09:30:51 +0530

    thb

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -30,55 +30,55 @@ def call() {
             }
 
         }
-        stage ('sonar check') {
-            steps {
-                script {
-                    env.ARGS = "-Dsonar.sources=."
-                    common.sonarCheck()
-                }
-            }
-        }
-        stage ('test cases') {
-            parallel {
-                stage('unit Tests') {
-                    steps {
-                        sh "echo Unit test cases completed"
-                    }
-                }
-                stage ('Integration Tests') {
-                    steps {
-                        sh "echo Integration test cases completed"
-                    }
-                }
-                stage('Functional Tests') {
-                    steps {
-                        sh " echo Functional test cases completed"
-                    }
-                }
-            }
-        }
-            stage('Prepare artifacts') {
-                when {
-                    expression { env.TAG_NAME != null }
-                }
-                steps {
-                    sh '''
-                        npm install
-                        zip ${COMPONENT}.zip node_modules server.js
-                    '''
-                }
-            }
-            stage('Upload artifacts') {
-                when {
-                    expression { env.TAG_NAME != null }
-                }
-                steps {
-                    sh '''
-                       curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file pom.xml http://172.31.3.52:8081/repository/${COMPONENT}/${COMPONENT}.zip
-                    '''
-                }
-            }
-        } 
-        }
-    }
+        //stage ('sonar check') {
+        //    steps {
+        //        script {
+        //            env.ARGS = "-Dsonar.sources=."
+        //            common.sonarCheck()
+        //        }
+        //    }
+        //}
+        //stage ('test cases') {
+        //    parallel {
+        //        stage('unit Tests') {
+        //            steps {
+        //                sh "echo Unit test cases completed"
+        //            }
+        //        }
+        //        stage ('Integration Tests') {
+        //            steps {
+        //                sh "echo Integration test cases completed"
+        //            }
+        //        }
+        //        stage('Functional Tests') {
+        //            steps {
+        //                sh " echo Functional test cases completed"
+        //            }
+       //         }
+       //     }
+        // }
+        //    stage('Prepare artifacts') {
+        //        when {
+        //            expression { env.TAG_NAME != null }
+        //        }
+        //        steps {
+        //            sh '''
+        //                npm install
+        //                zip ${COMPONENT}.zip node_modules server.js
+        //            '''
+        //        }
+        //    }
+        //    stage('Upload artifacts') {
+        //        when {
+        //            expression { env.TAG_NAME != null }
+        //        }
+        //        steps {
+        //            sh '''
+        //               curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file pom.xml http://172.31.3.52:8081/repository/${COMPONENT}/${COMPONENT}.zip
+        //            '''
+        //        }
+        //    }
+        //} 
+        //}
+    //}
 // call is the default function whicch will be called
