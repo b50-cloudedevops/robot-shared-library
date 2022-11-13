7a4bf184a2a7e24721fa42943d85381d5e8e0cf3
vamsi434 <vamsibusa@gmail.com>
2022-11-13 20:04:51 +0530

    thsg

--- vars/payment.groovy
+++ vars/payment.groovy
@@ -1,44 +1,57 @@
+//def call() {
+//   pipeline {
+//    agent any
+//      stages {
+//      stage('Lint checks') {
+//        steps {
+//          script {
+//            lintCheck()
+//      }
+//    }
+
+//        }
+//     stage('sonar check') {
+//            steps {
+//                script {
+//                    env.ARGS = "-Dsonar.sources=."
+//                    common.sonarCheck()
+//                }
+//            }
+//        }   
+
+//      stage('test cases') {
+//            parallel {
+//                stage('unit Tests') {
+//                   steps {
+//                        sh "echo Unit test cases completed"
+//                   }
+//                }
+//                stage('Integration Tests') {
+//                    steps {
+//                        sh "echo Integration test cases completed"
+//                    }
+//                }
+//                stage('Functional Tests') {
+//                    steps {
+//                        sh " echo Functional test cases completed"
+//                    }
+//                }
+//            }
+//        }        
+//    }
+//    }
+//}
+// call is the default function whicch will be called
 def call() {
-   pipeline {
-     agent any
-      stages {
-        stage('Lint checks') {
-            steps {
-                script {
-                    lintCheck()
-                }
-            }
-
+    node {
+        git branch: 'main', url: "https://github.com/b50-cloudedevops/${COMPONENT}.git"
+        env.APPTYPE="python"
+        common.sonarCheck()
+        common.lintCheck()
+        env.ARGS="-Dsonar.sources=."
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
         }
-     stage('sonar check') {
-            steps {
-                script {
-                    env.ARGS = "-Dsonar.sources=."
-                    common.sonarCheck()
-                }
-            }
-        }   
-
-      stage('test cases') {
-            parallel {
-                stage('unit Tests') {
-                    steps {
-                        sh "echo Unit test cases completed"
-                    }
-                }
-                stage('Integration Tests') {
-                    steps {
-                        sh "echo Integration test cases completed"
-                    }
-                }
-                stage('Functional Tests') {
-                    steps {
-                        sh " echo Functional test cases completed"
-                    }
-                }
-            }
-        }        
-    }
     }
 }
-// call is the default function whicch will be called
